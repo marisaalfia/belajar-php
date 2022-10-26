5557b422aa9cbcaa54ce65a1e164a34f1ba774d8
Marisa Alfia Suciati <marisauchie23@gmail.com>
2022-10-26 11:49:13 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama ="Marisa";
+$umur =20;
+
+echo "Nama Saya <strong>$nama, saya berusia $umur tahun.</br>";
+
+$namaAdik ="Rezta";
+$umurAdik ="19";
+
+echo "Nama  adik saya <strong>$namaAdik</strong>, adik saya berusia
+$umurAdik tahun.<br>";
+
+$selisihumur = $umur - $umurAdik;
+echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
+
+?>
