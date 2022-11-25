234bb17690748757ff1a909029c3bac2f8b2d0f7
Marisa Alfia Suciati <marisauchie23@gmail.com>
2022-11-25 23:28:16 +0700

    membuat index dan insert data

--- index.php
+++ index.php
@@ -1,19 +1,53 @@
 <?php
+//1. BUAT KONEKSI DENGAN MYSQL
+$con = mysqli_connect("localhost","root","","fakultas");
 
-echo "Hello World!<br>";
+//2. CEK KONEKSI DENGAN MYSQL
+if(mysqli_connect_errno()){
+    echo "Koneksi gagal". mysqli_connect_error();
+}else {
+    echo "Koneksi berhasil";
+}
 
-$nama ="Marisa";
-$umur =20;
+//3. MEMBACA DATA DARI TABEL MYSQL
+$query = "SELECT * FROM mahasiswa";
 
-echo "Nama Saya <strong>$nama</strong>, saya berusia $umur tahun.</br>";
-
-$namaAdik ="Rezta";
-$umurAdik ="19";
-
-echo "Nama  adik saya <strong>$namaAdik</strong>, adik saya berusia
-$umurAdik tahun.<br>";
-
-$selisihumur = $umur - $umurAdik;
-echo "Selisih usia saya dan adik saya adalah $selisihumur tahun.";
-
+//4. TAMPILKAN DATA, DENGAN MENJALANKAN SQL QUERY
+$result = mysqli_query($con, $query);
+$mahasiswa = [];
+if ($result){
+    // tampilkan data satu per satu
+    while($row = mysqli_fetch_assoc($result)){
+        $mahasiswa[]= $row;
+    }
+    mysqli_free_result($result);
+}
+//5. TUTUP KONEKSI MYSQL
+mysqli_close($con);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <!-- <?php var_dump($mahasiswa);?> -->
+    <table border="1" style="width:100%;">
+        <tr>
+            <th>NIM</th>
+            <th>Nama</th>
+        </tr>
+        <?php foreach($mahasiswa as $value) : ?>
+        <tr>
+            <td><?php echo $value["nim"]; ?></td>
+            <td><?php echo $value["nama"]; ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </table>  
+</body>
+</html>
